d85012b6cb185e723ac6a798b00db0afcb4cc9a4
root <root@DESKTOP-88U0KEK>
2023-07-08 14:03:03 +0200

    adding files

---
+++ e6.sh
@@ -0,0 +1,2 @@
+ls -l | awk 'NR % 2 == 1' | tail -n +2
+
---
+++ e7.sh
@@ -0,0 +1,4 @@
+export FT_LINE1=7; 
+export FT_LINE2=15; 
+cat /etc/passwd | grep -v '^#' | awk 'NR%2==0' | cut -d: -f1 | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,$/./'
+
